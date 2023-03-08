7476e68446322849747ad0d910b91a665786ed7c
Tschawi <89629023+Tschawi@users.noreply.github.com>
2023-03-08 13:56:09 +0100

    create table applications

---
+++ resources/views/Event.blade.php
@@ -0,0 +1,9 @@
+<form method="POST" action="">
+    <input value="" type="text" name="company" placeholder="Firma"><br>
+    <input value="" type="text" name="last_name" placeholder="Nachname"><br>
+    <input value="" type="text" name="first_name" placeholder="Vorname"><br>
+    <input value="" type="email" name="email_address" placeholder="Email"><br>
+    <input value="" type="text" name="job_title" placeholder="Job Titel"><br>
+    <input value="" type="text" name="business_phone" placeholder="Telefon"><br>
+    <button type="submit">Speichern</button>
+</form>
